3db6cbd3f51bd588a95a557ee4cfac987a08ba81
Mohit SIngla <mhtkumar46@gmail.com>
2024-09-12 16:34:51 +0530

    join room & send message

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,4 +1,4 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
 @WebSocketGateway({
@@ -8,14 +8,23 @@ import { Server, Socket } from "socket.io";
 })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server: Server;
-    
+
     handleConnection(client: Socket) {
         console.log('Client connected:', client.id);
         client.emit('connected', `You are connected! Your client ID is ${client.id}`);
     }
 
+    @SubscribeMessage('joinRoom')
+    async handleJoinRoom(client: Socket, payload: { userId: string, roomName: string }) {
+        client.join(payload.roomName);
+    }
+
+    @SubscribeMessage('message')
+    handleMessage(client: Socket, payload: { userId: string, roomName: string, message: string }) {
+        this.server.to(payload.roomName).emit('message', payload);
+    }
+
     handleDisconnect(client: Socket) {
         console.log('Client disconnected:', client.id);
     }
-
 }
